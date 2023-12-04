e4b77a8d858b856d2a29a74cba42c8ca0b0e7f54
Caroline Pereira <carolline.ps@hotmail.com>
2023-12-04 19:37:20 -0400

    update

--- src/app/heroes/mocks/Hero.mocks.ts
+++ src/app/heroes/mocks/Hero.mocks.ts
@@ -1,8 +1,9 @@
-import { Hero } from "src/app/Hero";
+import { Hero } from "../../Hero";
 
-export const HEROES: Hero[] = [
-    {id: 1, name: "Batman"},
-    {id: 2, name: "SuperMan"},
-    {id: 3, name: "Wolverine"},
-    {id: 4, name: "Deadpool"},
+const HEROES: Hero[] = [
+    {id: 1, name: "Thor", power: 89, img: "https://classic.exame.com/wp-content/uploads/2018/10/thor-ragnarok-filme-cultura-vip.jpg?quality=70&strip=info&w=720"},
+    {id: 2, name: "Hulk", power: 91, img:"https://img.olhardigital.com.br/wp-content/uploads/2023/06/o-incrivel-hulk-1.png"},
+    {id: 3, name: "Iron Man", power: 92, img:"https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"}
 ]
+
+export { HEROES };
